1cd4b8d60ae30e17fe986e035d55cdc372c89025
nickgoodmanbmstu <nikitakhoroshevsky@yandex.ru>
2021-09-24 09:32:43 +0300

    working correctly with '1' and '-1'

--- Source.c
+++ Source.c
@@ -1,6 +1,6 @@
 
 
-//works just as planned + beautified
+//works with negative numbers + beautified
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
@@ -14,25 +14,34 @@ int main() {
 	int i = 0;
 	//int x = 0;
 	scanf("%d", &v);
+	if (v == -1) printf("-1");
+	
+	
+	else
+	{ 
+		if (v == 1) printf("1");
+		else
+		{
+			do {
+				z = v;
+				v = v / 2;
+				a = 2 * v;
+				if (z == a) {
+					c[i] = 0;
+					//printf("0");
+					i++;
+				}
+				else {
 
-	do {
-		z = v;
-		v = v / 2;
-		a = 2 * v;
-		if (z == a) {
-			c[i] = 0;
-			//printf("0");
-			i++;
+					//printf("1");
+					c[i] = 1;
+					//printf("0");
+					i++;
+				}
+			} while (v > 1);
+			c[i] = v;
+			for (i; i >= 0; i--) printf("%d", c[i]);
 		}
-		else {
-
-			//printf("1");
-			c[i] = 1;
-			//printf("0");
-			i++;
-		}
-	} while (v > 1);
-	c[i] = v;
-	for (i; i >= 0; i--) printf("%d", c[i]);
+	}
 	return 0;
 }
