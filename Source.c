baa67456e893835c40cce6e7be38494f7c692ad5
nickgoodmanbmstu <nikitakhoroshevsky@yandex.ru>
2021-09-24 18:51:46 +0300

    last commit

--- Source.c
+++ Source.c
@@ -1,51 +1,58 @@
-
-
+//проверить значение, возвращаемое функцией scanf
 //works with negative numbers + beautified
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-
-int main() {
-
-	int v;
+int main(void) {
+	
+	int v = 0;
 	int z;
 	int a;
 	int c[64];
 	int i = 0;
-	//int x = 0;
-	scanf("%d", &v);
+	
+	
 
-	if (v < -32767) printf("Sorry, the number is too large or not a decimal number at all");
+	int b = scanf("%d", &v);
+	
+	if (b == 0) printf("Sorry, this is not a number");
+	else
+	{
 
-	else {
-		if (v > 32766) printf("Sorry, the number is too large  or not a decimal number at all");
+
+		if (v < -32767) printf("Sorry, the number is too large");
+
 		else {
-			if (v == 1) printf("1");
+			if (v > 32766) printf("Sorry, the number is too large");
 			else {
-				if (v == -1) printf("-1");
+				if (v == 1) printf("1");
 				else {
-					do {
-						z = v;
-						v = v / 2;
-						a = 2 * v;
-						if (z == a) {
-							c[i] = 0;
-							//printf("0");
-							i++;
-						}
-						else {
+					if (v == -1) printf("-1");
+					else {
+						do {
+							z = v;
+							v = v / 2;
+							a = 2 * v;
+							if (z == a) {
+								c[i] = 0;
+								//printf("0");
+								i++;
+							}
+							else {
 
-							//printf("1");
-							c[i] = 1;
-							//printf("0");
-							i++;
-						}
-					} while (v > 1);
-					c[i] = v;
-					for (i; i >= 0; i--) printf("%d", c[i]);
+								//printf("1");
+								c[i] = 1;
+								//printf("0");
+								i++;
+							}
+						} while (v > 1);
+						c[i] = v;
+						for (i; i >= 0; i--) printf("%d", c[i]);
+					}
 				}
 			}
 		}
+		return 0;
 	}
-	return 0;
+	
 }
