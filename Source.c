7e95a010e3322a2deef441921c45eb2aae1c3a70
nickgoodmanbmstu <nikitakhoroshevsky@yandex.ru>
2021-09-24 09:50:10 +0300

    added a 'number is too large' warning

--- Source.c
+++ Source.c
@@ -14,33 +14,44 @@ int main() {
 	int i = 0;
 	//int x = 0;
 	scanf("%d", &v);
-	if (v == -1) printf("-1");
+	
+	
+	if (v < -32767) printf("Sorry, the negative number is too large");
+
 	
 	
 	else
-	{ 
-		if (v == 1) printf("1");
+	{
+		if (v > 32766) printf("Sorry, the number is too large");
 		else
 		{
-			do {
-				z = v;
-				v = v / 2;
-				a = 2 * v;
-				if (z == a) {
-					c[i] = 0;
-					//printf("0");
-					i++;
-				}
-				else {
+			if (v == 1) printf("1");
+			else
+			{
+				if (v == -1) printf("-1");
+				else
+				{
+					do {
+						z = v;
+						v = v / 2;
+						a = 2 * v;
+						if (z == a) {
+							c[i] = 0;
+							//printf("0");
+							i++;
+						}
+						else {
 
-					//printf("1");
-					c[i] = 1;
-					//printf("0");
-					i++;
+							//printf("1");
+							c[i] = 1;
+							//printf("0");
+							i++;
+						}
+					} while (v > 1);
+					c[i] = v;
+					for (i; i >= 0; i--) printf("%d", c[i]);
 				}
-			} while (v > 1);
-			c[i] = v;
-			for (i; i >= 0; i--) printf("%d", c[i]);
+			}
 		}
 	}
 	return 0;
